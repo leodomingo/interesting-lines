1488e5640f0682fb2b6127fb41d6e12582c79fd8
leodomingo <adominguezleo@gmail.com>
2022-12-05 19:46:48 -0500

    maybe we plya on click

--- src/components/canvas/Key.svelte
+++ src/components/canvas/Key.svelte
@@ -8,68 +8,61 @@
 	export let canPlay;
 	export let volumeCap;
 
-    let hasPlayed = false;
+	let hasPlayed = false;
 
 	export let volume;
-    let volumeTicker;
-
-
-
+	let volumeTicker;
 
 	let audioCtrl;
 
+	const fadeKey = () => {
+		volumeTicker = setInterval(() => {
+			if (active && volume < volumeCap) {
+				if (audioCtrl.paused) {
+					audioCtrl.play();
+				}
+				let inc = (volume += 0.05);
+				inc = limits(inc, 0, 1);
+				volume = inc;
+			} else if (!active && volume > 0) {
+				let dec = (volume -= 0.05);
+				dec = limits(dec, 0, 1);
+				volume = dec;
+			}
+		}, 50);
+	};
 
-    const fadeKey = ()=>{
-        if(audioCtrl.paused){
-            audioCtrl.play();
-        }
-         volumeTicker =  setInterval(() => {
-            if(active && volume < volumeCap){
-                let inc = volume +=0.05
-                    inc = limits(inc,0,1)
-					volume = inc;
-            }else if(!active && volume > 0) {
-                let dec = volume -=0.05
-                    dec = limits(dec,0,1)
-					volume = dec;
-            }
-        },50);
-    }
+	const resetAudio = () => {
+		if (audioCtrl.currentTime > 7) {
+			audioCtrl.currentTime = 0;
+		}
+	};
 
-    const resetAudio = ()=>{
-        if(audioCtrl.currentTime > 7){
-                audioCtrl.currentTime = 0;
-            }
-    }
+	onMount(() => {
+		//   audioCtrl.volume = 0;
+		audioCtrl.addEventListener('timeupdate', resetAudio);
 
-    onMount(() => {
-     //   audioCtrl.volume = 0;
-        audioCtrl.addEventListener('timeupdate',resetAudio);
-
-        return ()=>{
-            clearInterval(volumeTicker);
-            audioCtrl.removeEventListener('timeupdate', resetAudio)
-        }
-
-    });
-
+		return () => {
+			clearInterval(volumeTicker);
+			audioCtrl.removeEventListener('timeupdate', resetAudio);
+		};
+	});
 
 	$: {
 		if (canPlay) {
-           // fadeKey();
-            hasPlayed = true;
+			fadeKey();
+			hasPlayed = true;
 		}
 	}
 
 	$: {
 		if (audioCtrl) {
-            if(volume == 0){
-                audioCtrl.currentTime = 0.5;
-            }
+			if (volume == 0) {
+				audioCtrl.currentTime = 0.5;
+			}
 			audioCtrl.volume = limits(volume, 0, 1);
 		}
 	}
-
 </script>
 
 <div>
@@ -79,8 +72,5 @@
 	</audio>
 </div>
 
-
-
 <style>
-
 </style>
